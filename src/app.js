0b895c86fe1da51f7a27e60a82c1cc307c534f28
cephscope <sworm.null@gmail.com>
2018-07-16 13:55:41 -0400

    remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -11,14 +11,6 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'WATER BILL', amount: 54981657681 }));
-store.dispatch(addExpense({ description: 'GAS BILL', createdAt: 1530547200000 }));
-store.dispatch(addExpense({ description: 'RENT', amount: 51984154717 }));
-
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
 
 //Provider API usage. Pass prop "store" setting it equal to your apps redux store
 const jsx = (
