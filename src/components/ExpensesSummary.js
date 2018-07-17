3e8c38a9de32a8948f3df3d2b6d7d9a115ee8a34
cephscope <sworm.null@gmail.com>
2018-07-16 22:20:24 -0400

    refactored Expenses Summary

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,30 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import numeral from 'numeral';
-import expensesCount from '../selectors/expenses';
+import selectExpenses from '../selectors/expenses';
 import expensesTotal from '../selectors/expenses-total';
 
-export class ExpensesSummary extends React.Component {
-  checkGrammar = () => {
-    if (this.props.expenses.length === 1) {
-      return `Viewing ${this.props.expenses.length} expense totaling ${numeral(expensesTotal(this.props.expenses) / 100).format('$0,0.00')}`
-    } else {
-      return `Viewing ${this.props.expenses.length} expenses totaling ${numeral(expensesTotal(this.props.expenses) / 100).format('$0,0.00')}`
-    }
-  }
-  
-  render() {
-    return (
-      <div>
-        <h3>{this.checkGrammar()}</h3>
-      </div>
-    );
-  }
-}
-  
+export const ExpensesSummary = ({ visibleExpenses }) => {
+  const expenseWord = visibleExpenses.length === 1 ? 'expense' : 'expenses';
+  const formattedExpensesTotal = numeral(expensesTotal(visibleExpenses) / 100).format('$0,0.00');
+
+  return (
+    <div>
+      <h3>{`Viewing ${visibleExpenses.length} ${expenseWord} totaling ${formattedExpensesTotal}`}</h3>
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => ({
-  expenses: expensesCount(state.expenses, state.filters)
+  visibleExpenses: selectExpenses(state.expenses, state.filters)
 });
 
 export default connect(mapStateToProps)(ExpensesSummary);
--- src/tests/components/ExpensesSummary.test.js
+++ src/tests/components/ExpensesSummary.test.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import numeral from 'numeral';
 import { ExpensesSummary } from '../../components/ExpensesSummary';
 import expenses from '../fixtures/expenses';
 
 test('should return correct message for multiple expenses', () => {
-  const wrapper = shallow(<ExpensesSummary expenses={expenses}/>);
+  const wrapper = shallow(<ExpensesSummary visibleExpenses={expenses}/>);
   expect(wrapper).toMatchSnapshot();
 });
 
@@ -17,6 +16,6 @@ test('should return correct message for one expense', () => {
     amount: 195,
     createdAt: 0
   }];
-  const wrapper = shallow(<ExpensesSummary expenses={expense}/>);
+  const wrapper = shallow(<ExpensesSummary visibleExpenses={expense}/>);
   expect(wrapper).toMatchSnapshot();
 });
